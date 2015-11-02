34eece59ad10a5b6189f4916a990b7346e2f0acd
Muhammad-Imran <muhammad.imran@globalnorthstar.com>
2015-11-02 18:47:54 +0500

    Injecting objects into spring xml

--- src/org/koushik/javabrains/Triangle.java
+++ src/org/koushik/javabrains/Triangle.java
@@ -1,36 +1,38 @@
 package org.koushik.javabrains;
 
 public class Triangle {
-	private String type;
-	private int height;
 	
-	public Triangle(String type){
-		this.type = type;
-	}
-	
-	public Triangle(int height){
-		this.height = height;
-	}
-	
-	public Triangle(String type, int height){
-		this.type = type;
-		this.height = height;
-	}
-	
-	public String getType() {
-		return type;
-	}
-
-	/*public void setType(String type) {
-		this.type = type;
-	}*/
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 	
 	public void draw(){
-		System.out.println(getType() + " Triangle drawn of height " + getHeight());
+		System.out.println("Point A =( " + getPointA().getX() + ", " + getPointA().getY() + ")");
+		System.out.println("Point B =( " + getPointB().getX() + ", " + getPointB().getY() + ")");
+		System.out.println("Point C =( " + getPointC().getX() + ", " + getPointC().getY() + ")");
 	}
 
-	public int getHeight() {
-		return height;
+	public Point getPointA() {
+		return pointA;
 	}
 
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
 }
